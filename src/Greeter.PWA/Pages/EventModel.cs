a6f43800e073cf4e6478dff88facadaa25ff1014
Sysinfocus <info@sysinfocus.com>
2025-06-22 17:08:40 +0530

    Greeter app with PWA

---
+++ src/Greeter.PWA/Pages/EventModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Greeter.PWA.Pages;
+public class EventModel
+{
+    public string Name { get; set; } = string.Empty;
+    public string Mobile { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public string Message { get; set; } = string.Empty;
+}
